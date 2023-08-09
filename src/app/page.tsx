b6adb01ef461769d4beb884b20c4ff3ccdf62623
DrizzyLee <drizz.lee1@gmail.com>
2023-08-10 00:13:30 +0200

    create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
 import { TextInput, Card, Button, Checkbox } from "@aomdev/ui";
 
 export default function Home() {
   return (
     <>
-      <Button>Hello</Button>
+    <div className="cardcontainer">
+    <h1 className="font-heading font-medium text-3xl">Connexion</h1>
+      <Card className="card">
+        <TextInput label="Nom" required />
+        <TextInput label="Prénom" required />
+        <TextInput label="Plaque d'immatriculation" required />
+        <Checkbox label="Se souvenir de moi" />
+        <Button fullWidth size="sm" className="rounded-full mt-6">
+        Suivant
+        </Button>
+      </Card>
+    </div>
     </>
   );
 }
