74427bd7c80568eca91b0e0edfe6dd15204482ac
DrizzyLee <drizz.lee1@gmail.com>
2023-08-10 22:11:53 +0200

    create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { TextInput, Card, Button, Checkbox } from "@aomdev/ui";
+import { TextInput, Card, Button, Checkbox, Select, ActionIcon } from "@aomdev/ui";
+import { IconPencil } from "@tabler/icons-react";
 import { useState } from "react";
 
 export default function Home() {
@@ -29,7 +30,49 @@ export default function Home() {
           </Card>
         </div>
       )}
-      {step === 1 && <></>}
+      {step === 1 && (
+        <>
+          <div className="bg-gradient-to-b from-tertiary-400 fixed  top-0 left-0 w-full to-primary-500 py-10 rounded-b-3xl">
+            <Card className="cardInfo">
+              <div className="flex justify-between">
+                <p className="text-sm text-gray-600 font-medium">{new Date().toDateString()}</p>
+                <ActionIcon>
+                  <IconPencil size={"75%"} />
+                </ActionIcon>
+              </div>
+              <div className="flex justify-between">
+                <p className="font-medium text-gray-800">Kareem Lewis</p>
+                <p className="text-gray-800 font-medium">4302-AAC</p>
+              </div>
+            </Card>
+          </div>
+          <div className="container">
+            <div className="divContainer">
+              <div className="tasksfiltre">
+                <Select
+                  defaultValue="all"
+                  items={[
+                    { label: "Tout", value: "all" },
+                    { label: "Complété", value: "completed" },
+                    { label: "Interminé", value: "incomplete" }
+                  ]}
+                />
+                <span>3/5 tâches</span>
+              </div>
+              <div className="CARD">
+                <Card className="card2">Vérif pare-brise</Card>
+                <Card className="card2">Vérif pneus</Card>
+                <Card className="card2">Vérif rétroviseurs</Card>
+                <Card className="card2">Vérif portes</Card>
+                <Card className="card2">Vérif moteur</Card>
+              </div>
+              <Button onClick={next} fullWidth size="sm" className="rounded-full mt-6">
+                Suivant
+              </Button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
