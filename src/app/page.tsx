0aff932c8db2f76bd173cecd4b40dd402439f611
Agis Carty <a.carty2555@gmail.com>
2023-08-10 13:10:12 -0400

    [chore] - setup second part of project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
 "use client";
 import { TextInput, Card, Button, Checkbox } from "@aomdev/ui";
+import { useState } from "react";
 
 export default function Home() {
+  const [step, setStep] = useState(0);
+
+  const next = () => {
+    setStep(1);
+  };
+
+  const previous = () => {
+    setStep(0);
+  };
+
   return (
     <>
-    <div className="cardcontainer">
-    <h1 className="font-heading font-medium text-3xl">Connexion</h1>
-      <Card className="card">
-        <TextInput label="Nom" required />
-        <TextInput label="Prénom" required />
-        <TextInput label="Plaque d'immatriculation" required />
-        <Checkbox label="Se souvenir de moi" />
-        <Button fullWidth size="sm" className="rounded-full mt-6">
-        Suivant
-        </Button>
-      </Card>
-    </div>
+      {step === 0 && (
+        <div className="cardcontainer">
+          <h1 className="font-heading font-medium text-3xl">Connexion</h1>
+          <Card className="card">
+            <TextInput label="Nom" required />
+            <TextInput label="Prénom" required />
+            <TextInput label="Plaque d'immatriculation" required />
+            <Checkbox label="Se souvenir de moi" />
+            <Button onClick={next} fullWidth size="sm" className="rounded-full mt-6">
+              Suivant
+            </Button>
+          </Card>
+        </div>
+      )}
+      {step === 1 && <></>}
     </>
   );
 }
