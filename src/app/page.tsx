3cdf14ba083e4d768656c94f6b2e6e99ee2ae742
DrizzyLee <drizz.lee1@gmail.com>
2023-08-11 21:52:55 +0200

    recapPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
 
   const next = () => {
     if (step === 2) return;
-    setStep(prev => prev + 1);
+    setStep((prev) => prev + 1);
   };
 
   const previous = () => {
     if (step === 1) return;
-    setStep(prev => prev - 1);
+    setStep((prev) => prev - 1);
   };
 
   return (
@@ -77,13 +77,48 @@ export default function Home() {
       )}
       {step === 2 && (
         <>
-          <div className="flex gap-2">
-            <Button fullWidth size="sm" className="rounded-full mt-6">
-              Valider
-            </Button>
-            <Button variant={"neutral"} onClick={previous} fullWidth size="sm" className="rounded-full mt-6">
-              Retour
-            </Button>
+          <div className="bg-gradient-to-b from-tertiary-400 fixed  top-0 left-0 w-full to-primary-500 py-10 rounded-b-3xl">
+            <Card className="cardInfo">
+              <div className="flex justify-between">
+                <p className="text-sm text-gray-600 font-medium">{new Date().toDateString()}</p>
+                <ActionIcon>
+                  <IconPencil size={"75%"} />
+                </ActionIcon>
+              </div>
+              <div className="flex justify-between">
+                <p className="font-medium text-gray-800">Kareem Lewis</p>
+                <p className="text-gray-800 font-medium">4302-AAC</p>
+              </div>
+            </Card>
+          </div>
+          <div className="rec-contain">
+            <div className="rec-contain2">
+              <h1 className="recap">
+                <strong>Récapitulatif</strong>
+              </h1>
+              <div className="recapCard">
+                <Card className="recap2">Fluide de clignotant</Card>
+                <Card className="recap2">Freins</Card>
+                <Card className="recap2">Pare-brise</Card>
+                <Card className="recap2">Rétroviseurs</Card>
+                <Card className="recap2">Pneus</Card>
+              </div>
+
+              <div className="flex gap-2">
+                <Button fullWidth size="sm" className="rounded-full mt-6 grow">
+                  Valider
+                </Button>
+                <Button
+                  variant={"neutral"}
+                  onClick={previous}
+                  fullWidth
+                  size="sm"
+                  className="rounded-full mt-6 grow"
+                >
+                  Retour
+                </Button>
+              </div>
+            </div>
           </div>
         </>
       )}
