c9497c1f8dc43c4bc12eb6ccdabdb21adc26f4c0
Agis Carty <a.carty2555@gmail.com>
2023-08-14 09:59:42 -0400

    small fix

--- src/app/api/user_checks/route.ts
+++ src/app/api/user_checks/route.ts
@@ -1,7 +1,11 @@
 import { admin } from "@/lib/supabase";
 
+type RequestData = {
+  data: any[];
+};
+
 export const PUT = async (req: Request) => {
-  const updatedChecks = await req.json();
+  const updatedChecks = (await req.json()) as RequestData;
 
   console.log(updatedChecks.data);
   // admin.from('user_checks').update()
